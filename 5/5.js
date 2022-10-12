173d7d35c4b6107b53a9ff347b1922b130dac399
VladimirovKirill <113458654+VladimirovKirill@users.noreply.github.com>
2022-10-12 18:40:00 +0300

    Five

---
+++ 5/5.js
@@ -0,0 +1,36 @@
+function CalculatedPrice() {
+
+    let Price = document.getElementsByName("Price");
+    let Count = document.getElementsByName("Count");
+    let Result = document.getElementById("Result");
+
+    let toPrice = Price[0].value;
+    let toCount = Count[0].value;
+
+    let toCountMatch = toCount.match(/^\d+$/);
+    let toPriseMatch = toPrice.match(/^\d+$/);
+
+    if (toCountMatch !== null && toPriseMatch !== null) {
+    result.innerHTML = parseInt(Price[0].value) * parseInt(Count[0].value);
+    }
+
+    else if (toCountMatch !== null && toPriseMatch === null ){
+    result.innerHTML = "Введите цену товара!";
+    }
+
+    else if (toCountMatch === null && toPriseMatch !== null) {
+      result.innerHTML = "Введите количество товара!";
+    }
+
+    else if (toCountMatch === null && toPriseMatch === null) {
+      result.innerHTML = "Некорректно заполненная форма, пожалуйста, проверьте вписанное и повторите попытку снова!";
+    }
+
+    return false;
+  }
+
+  window.addEventListener("DOMContentLoaded", function (event) {
+    console.log("DOM fully loaded and parsed");
+    let b = document.getElementById("calculateResult");
+    b.addEventListener("click", onClick);
+  });
